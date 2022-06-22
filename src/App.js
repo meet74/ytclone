112c99ccd3388d548d5ead5f5acd02075cd2236f
meet74 <meet237@gmail.com>
2022-06-22 14:04:08 +0530

    video detail page done

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Navbar from './components/Navbar/Navbar';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import React,{useState, useEffect} from 'react'
 import useCheckMobileScreen from './hooks/useCheckMobileScreen';
+import VideoDetail from './pages/VideoDetail/VideoDetail';
 function App() {
   const [images, setimages] = useState([]);
  
   useEffect(() => {
 
-    fetch('https://picsum.photos/v2/list', {
+    fetch('https://api.unsplash.com/photos/?client_id=hCWk_IEBVMEVa0iluSGK6yUkSn0bIUP8NVy1K0RvI9k', {
       method: "GET"
     }).then(async res => {
       const apiImages = await res.json();
@@ -19,53 +20,57 @@ function App() {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <VideoDetail/>
+    // <div className="App">
+    //   <header className="App-header">
       
-       <Navbar/>
+    //    <Navbar/>
        
         
-      </header>
-      <main>
-      <div className='grid-container'>
-      {images.map((item, index) => {
-        return <div className='grid-item'>
-          <picture>
-            <img src={item.download_url} style={{
-              height: "175px",
-              width: "300px"
-            }} />
-            <div className='length-text'>
-              <p>1:45</p>
-            </div>
-          </picture>
-          <div style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}>
-            <img src={item.download_url} style={{
-              height: "30px",
-              width: "30px",
-              borderRadius: "50px"
-            }} />
-            <p style={{
-              fontFamily: "Roboto",
-              fontSize: "1rem",
-              textAlign: "start",
-              margin: "5px 5px 5px 15px"
-            }}>Use the Most popular Crosshair in Valorant</p>
-            <MoreVertIcon style={{
-              color: "white",
-              width: "25px",
-              height: "25px"
-            }} />
-          </div>
-        </div>;
-      })}
-    </div>
-      </main>
-    </div>
+    //   </header>
+    //   <main>
+    //   <div className='grid-container'>
+    //   {images.map((item, index) => {
+    //     return <div className='grid-item'>
+    //       <picture>
+    //         <img src={item.urls.regular} style={{
+    //           height: "175px",
+    //           width: "300px",
+
+    //         }} />
+    //         <div className='length-text'>
+    //           <p>1:45</p>
+    //         </div>
+    //       </picture>
+    //       <div style={{
+    //         display: "flex",
+    //         alignItems: "center",
+    //         justifyContent: "center",
+    //         width:"100%",
+           
+    //       }}>
+    //         <img src={item.urls.regular} style={{
+    //           height: "30px",
+    //           width: "30px",
+    //           borderRadius: "50px"
+    //         }} />
+    //         <p style={{
+    //           fontFamily: "Roboto",
+    //           fontSize: "1rem",
+    //           textAlign: "start",
+    //           margin: "5px 5px 5px 15px"
+    //         }}>Use the Most popular Crosshair in Valorant</p>
+    //         <MoreVertIcon style={{
+    //           color: "white",
+    //           width: "25px",
+    //           height: "25px"
+    //         }} />
+    //       </div>
+    //     </div>;
+    //   })}
+    // </div>
+    //   </main>
+    // </div>
   );
 }
 
---
+++ src/pages/VideoDetail/VideoDetail.js
@@ -0,0 +1,78 @@
+import React,{useState,useEffect} from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import './VideoDetail.css'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+function VideoDetail() {
+    const [images, setimages] = useState([]);
+ 
+  useEffect(() => {
+
+    fetch('https://api.unsplash.com/photos/?client_id=hCWk_IEBVMEVa0iluSGK6yUkSn0bIUP8NVy1K0RvI9k', {
+      method: "GET"
+    }).then(async res => {
+      const apiImages = await res.json();
+      setimages(apiImages);
+    })
+  }, [])
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <Navbar />
+        <main className="main-body">
+          <div className="video-box" >
+          <div
+          className="video-container"
+           
+          >
+            <h1>video</h1>
+          </div>
+          <h4>Use the Most popular Crosshair in Valorant</h4>
+          
+          </div>
+          <div style={{display:"flex",flex:1}}>
+          <div className='grid-container'>
+       {images.map((item, index) => {
+         return <div className='grid-item'>
+           <picture>
+             <img src={item.urls.regular} style={{
+               height: "100px",
+               width: "175px",
+
+             }} />
+             <div className='length-text'>
+               <p>1:45</p>
+             </div>
+           </picture>
+           <div style={{
+             display: "flex",
+             width:"100%",
+           
+           }}>
+            
+             <p style={{
+               fontFamily: "Roboto",
+               fontSize: "0.9rem",
+               textAlign: "start",
+               margin: "5px 5px 5px 15px",
+               width:"200   px"
+             }}>Use the Most popular Crosshair in Valorant</p>
+             <MoreVertIcon style={{
+               color: "white",
+               width: "20px",
+               height: "20px",
+               margin:"15px"
+             }} />
+           </div>
+         </div>;
+       })}
+     </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default VideoDetail;
